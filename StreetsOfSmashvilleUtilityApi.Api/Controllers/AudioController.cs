7378fb5a432dc2be6e786c4b770cc9e79b1408c6
Cody Sirk <csirk005@gmail.com>
2019-08-28 15:25:52 -0500

    api endpoint for workout assistant ready to test

--- StreetsOfSmashvilleUtilityApi.Api/Controllers/AudioController.cs
+++ StreetsOfSmashvilleUtilityApi.Api/Controllers/AudioController.cs
@@ -3,17 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Speech.Synthesis;
 using System.Web.Http;
+using Wavify.Core.Actions;
 
 namespace StreetsOfSmashvilleUtilityApi.Api.Controllers
 {
     public class AudioController : ApiController
     {
-        [HttpGet]
-        [Route("getvoices")]
-        public void GetCollection()
+        [HttpPost]
+        [Route("getMp3BytesForSpeechPromptCollection")]
+        public HttpResponseMessage GetMp3BytesForSpeechPromptCollection([FromBody] SpeechPromptCollection speechPromptCollection)
         {
-            
+            var promptBuilder = new PromptBuilder();
+
+            foreach (var speakPause in speechPromptCollection.SpeakPauseCollection)
+            {
+                var speakSection = speakPause.Key;
+                var pauseTime = speakPause.Value;
+
+                promptBuilder.AppendText(speakSection);
+                AppendBreakToPrompt(promptBuilder, pauseTime);
+            }
+
+            var mp3ByteArray = SpeechAction.ConvertSpeechSynthPromptToMp3ByteArray(promptBuilder);
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new ByteArrayContent(mp3ByteArray)
+            };
+
+            result.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment")
+            {
+                FileName = $"Workout_Assistant_{DateTime.Now}_Workout.mp3"
+            };
+
+            //result.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+
+            return result;
         }
+
+
+        private PromptBuilder AppendBreakToPrompt(PromptBuilder prompt, int seconds)
+        {
+            prompt.AppendBreak(new TimeSpan(0, 0, seconds));
+            return prompt;
+        }
+    }
+
+
+    public class SpeechPromptCollection
+    {
+        public Dictionary<string, int> SpeakPauseCollection { get; set; }
     }
 }
--- StreetsOfSmashvilleUtilityApi.Api/Global.asax.cs
+++ StreetsOfSmashvilleUtilityApi.Api/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -13,11 +14,33 @@ namespace StreetsOfSmashvilleUtilityApi.Api
     {
         protected void Application_Start()
         {
+            // Sometimes files aren't loaded properly from bin. Hint to the app to load from /bin, too.
+            CheckAddBinPath();
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        /// <summary>
+        /// Updates PATH variable in hosting instance to allow referring to items in this project's /bin folder.
+        /// Very helpful with Azure.
+        /// </summary>
+        public static void CheckAddBinPath()
+        {
+            // find path to 'bin' folder
+            var binPath = Path.Combine(new string[] { AppDomain.CurrentDomain.BaseDirectory, "bin" });
+            // get current search path from environment
+            var path = Environment.GetEnvironmentVariable("PATH") ?? "";
+
+            // add 'bin' folder to search path if not already present
+            if (!path.Split(Path.PathSeparator).Contains(binPath, StringComparer.CurrentCultureIgnoreCase))
+            {
+                path = string.Join(Path.PathSeparator.ToString(), new string[] { path, binPath });
+                Environment.SetEnvironmentVariable("PATH", path);
+            }
         }
     }
 }
